d30961f1d629d26a0ae7b5f69ee3c1cec20e5b1d
EsdrasAmora <esdras.amora@gmail.com>
2021-03-21 19:21:14 -0300

    cleanup

--- src/api/user/user.resolver.ts
+++ src/api/user/user.resolver.ts
@@ -1,8 +1,8 @@
 import { Arg, Mutation, Resolver } from 'type-graphql';
 import { Service } from 'typedi';
 import { CreateUserUseCase } from '@domain/create-user.use-case';
-import { UserInput } from '@domain/user.input';
-import { CreateUserResponse } from '@domain/create-user.response';
+import { CreateUserInput } from '@api/user/user.input';
+import { CreateUserResponse } from '@api/user/create-user.response';
 
 @Service()
 @Resolver()
@@ -10,7 +10,7 @@ export class UserResolver {
   constructor(private createUseCase: CreateUserUseCase) {}
 
   @Mutation(() => CreateUserResponse, { description: 'Create user' })
-  createUser(@Arg('data') data: UserInput): Promise<CreateUserResponse> {
+  createUser(@Arg('data') data: CreateUserInput): Promise<CreateUserResponse> {
     return this.createUseCase.exec(data);
   }
 }
