e16550f1b3f298decd8c4305d2c4ef3908898a8b
EsdrasAmora <esdras.amora@gmail.com>
2021-04-03 17:09:05 -0300

    user seed

---
+++ src/data/db/user.seed.ts
@@ -0,0 +1,27 @@
+import * as Faker from 'faker';
+import { Service } from 'typedi';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { CryptoService } from '@core/crypto.service';
+import { UserEntity } from '@data/entities';
+
+@Service()
+export class UserSeed {
+  constructor(
+    @InjectRepository(UserEntity) private readonly dbOrmRepository: Repository<UserEntity>,
+    private readonly cryptoService: CryptoService,
+  ) {}
+
+  async exec() {
+    const users = new Array(50).fill(0).map(() => {
+      const fakeUser = new UserEntity();
+      fakeUser.name = Faker.name.findName();
+      fakeUser.email = Faker.internet.email();
+      fakeUser.salt = this.cryptoService.generateRandomPassword();
+      fakeUser.password = this.cryptoService.generateHashWithSalt('1234qwer', fakeUser.salt);
+      return fakeUser;
+    });
+
+    await this.dbOrmRepository.save(users);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { loadApp } from '@api';
+import { UserSeed } from '@data/db/user.seed';
+
+import Container from 'typedi';
 
 declare let run: any;
 process.on('unhandledRejection', (up) => {
@@ -6,9 +9,14 @@ process.on('unhandledRejection', (up) => {
 });
 
 const isTest = process.argv[1].indexOf('mocha') >= 0;
+const isSeed = process.argv[2] == 'seed:user';
 
-loadApp().then(() => {
+loadApp().then((server) => {
   if (isTest) {
     run();
+  }
+  if (isSeed) {
+    const seed = Container.get(UserSeed);
+    seed.exec().then(() => server.close());
   }
 });
