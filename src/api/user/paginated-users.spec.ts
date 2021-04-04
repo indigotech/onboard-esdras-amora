8044735ba556d4e5318d20aa75a6df5f7baa79e1
EsdrasAmora <esdras.amora@gmail.com>
2021-04-04 20:22:21 -0300

    tests

---
+++ src/api/user/paginated-users.spec.ts
@@ -0,0 +1,93 @@
+import Container from 'typedi';
+import { getRepository, Repository } from 'typeorm';
+import { expect } from 'chai';
+import { gql } from 'apollo-server-express';
+import { PageInfoResponseFragment } from '@api/common/page-info.type';
+import { RequestMaker } from '@test/request-maker';
+import { PageInputModel, Paginated } from '@core/pagination';
+import { StatusCode } from '@core/error';
+import { UserEntity } from '@data/db/entities';
+import { UserSeed } from '@data/db/user.seed';
+import { UserResponse } from './create-user.response';
+import { UserResponseFragment } from './user.fragment';
+
+describe('GraphQL - UserResolver - PaginatedUsers', () => {
+  let repository: Repository<UserEntity>;
+  let requestMaker: RequestMaker;
+  let userSeed: UserSeed;
+
+  const paginatedUsersQuery = gql`
+    ${UserResponseFragment}
+    ${PageInfoResponseFragment}
+    query users($data: PageInput!) {
+      users(data: $data) {
+        count
+        nodes {
+          ...UserResponse
+        }
+        pageInfo {
+          ...PageInfoResponse
+        }
+      }
+    }
+  `;
+
+  before(() => {
+    requestMaker = new RequestMaker();
+    requestMaker.refreshAuth();
+    repository = getRepository(UserEntity);
+    userSeed = Container.get(UserSeed);
+  });
+
+  afterEach(async () => {
+    await repository.delete({});
+  });
+
+  it('should return a paginated list of users', async () => {
+    const total = 50;
+    const page = 3;
+    const limit = 12;
+    const offset = limit * page;
+    await userSeed.exec(total);
+
+    const input = {
+      data: { limit, page },
+    };
+
+    const response = await requestMaker.postGraphQL<{ users: Paginated<UserResponse> }, { data: PageInputModel }>(
+      paginatedUsersQuery,
+      input,
+    );
+    const data = response.body.data;
+    expect(data?.users.nodes.length).to.be.eq(limit);
+    expect(data?.users.pageInfo).to.be.deep.eq({
+      page: page,
+      offset,
+      limit: limit,
+      totalPages: Math.ceil(total / limit),
+      hasNextPage: total > offset + limit,
+      hasPreviousPage: offset > 0,
+    });
+
+    const [dbUsers, count] = await repository.findAndCount({ take: limit, skip: offset, order: { name: 'ASC' } });
+    expect(dbUsers.length).to.be.eq(data?.users.nodes.length);
+    expect(count).to.be.eq(data?.users.count);
+    dbUsers.map(({ id, email, name }, index) => {
+      expect({ id, email, name }).to.be.deep.eq(data?.users.nodes[index]);
+    });
+  });
+
+  it('Should return a error if any arg is less than 0', async () => {
+    await userSeed.exec();
+    const input = {
+      data: { limit: -1, page: 0 },
+    };
+
+    const response = await requestMaker.postGraphQL<{ users: Paginated<UserResponse> }, { data: PageInputModel }>(
+      paginatedUsersQuery,
+      input,
+    );
+    expect(response.body.errors?.[0].code).to.be.eq(StatusCode.BadRequest);
+    expect(response.body.errors?.[0].message).to.be.eq('Argument Validation Error');
+  });
+});
